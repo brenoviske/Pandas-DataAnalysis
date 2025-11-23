52df72da034c1a3ad254993544bceb68d86e9016
Breno Reis <reisb6774@gmail.com>
2025-11-23 17:35:02 -0300

    feat:Just some alterations in the code

--- ML/diceGameML/ml.py
+++ ML/diceGameML/ml.py
@@ -1,4 +1,4 @@
-# FOR MACHINE LEANRING ALGORITHMS
+# FOR MACHINE LEARNING ALGORITHMS
 import numpy as np
 from app import df
 from sklearn.metrics import mean_absolute_error , mean_squared_error , r2_score
